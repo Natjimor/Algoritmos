18dbaa1d78c500ef65f1bca9f88592fee7fda388
Natjimor <125272271+Natjimor@users.noreply.github.com>
2023-09-29 19:01:39 -0500

    Delete src directory

--- src/services/data.ts
+++
@@ -1,9 +0,0 @@
-export async function get_data() {
-    try {
-        const data = await fetch('https://rickandmortyapi.com/api/character').then (res => res.json());
-        console.log(data);
-        return data.results
-    } catch (error) {
-       console.log(error); 
-    }
-}
--- src/types/api_type.ts
+++
@@ -1,6 +0,0 @@
-export interface api_type {
-    name:string;
-    status:string;
-    species:string;
-    image:string;
-}

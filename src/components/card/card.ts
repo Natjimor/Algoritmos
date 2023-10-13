fae2121bcc64fbf757b527b7054ee00580f46709
Natjimor <125272271+Natjimor@users.noreply.github.com>
2023-10-13 15:00:23 -0500

    Update card.ts

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -4,7 +4,7 @@ export enum episode_props {
 	'name' = 'name',
 	'air_date' = 'air_date',
 	'episode' = 'episode',
-	'character' = 'character'
+	'characters' = 'characters'
 }
 
 export default class Card extends HTMLElement {
@@ -12,7 +12,7 @@ export default class Card extends HTMLElement {
 		name: '',
 		air_date: '',
 		episode:'',
-		character: ''
+		characters: ''
 	}
 
 	static get observedAttributes() {
@@ -20,7 +20,7 @@ export default class Card extends HTMLElement {
 			name: null,
 			air_date: null,
 			episode:null,
-			character: null
+			characters: null
 		};
 		return Object.keys(attrs);
 	}
@@ -39,8 +39,8 @@ export default class Card extends HTMLElement {
 				this.properties.episode = newValue
 				break;
 
-			case episode_props.character:
-				this.properties.character = newValue
+			case episode_props.characters:
+				this.properties.characters = newValue
 				break;
 			default:
 				break;
@@ -62,7 +62,7 @@ export default class Card extends HTMLElement {
 			<h2>${this.properties.name}</h2>
 			<p>${this.properties.air_date}</p>
 			<p>${this.properties.episode}</p>
-			<p>${this.properties.character}</p>
+			<p>${this.properties.characters}</p>
         `;
 	}
 }
